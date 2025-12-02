495e15420a8ca14a397ba939290cffe8067580d2
David Paez <jeffersonpaez5@gmail.com>
2025-12-02 19:53:06 -0400

    Creacion Login Sin codigo, solo estetico

--- CapaPresentacion/Login.Designer.cs
+++ CapaPresentacion/Login.Designer.cs
@@ -28,19 +28,181 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            panel2 = new Panel();
+            txtUsuario = new TextBox();
+            txtPassword = new TextBox();
+            panel3 = new Panel();
+            lblLogin = new Label();
+            btnAcceder = new Button();
+            btnCerrar = new PictureBox();
+            btnMinimizar = new PictureBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)btnCerrar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)btnMinimizar).BeginInit();
             SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(40, 167, 69);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(200, 280);
+            panel1.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.LogoLogin;
+            pictureBox1.Location = new Point(0, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(198, 256);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.MouseDown += pictureBox1_MouseDown;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.DimGray;
+            panel2.Enabled = false;
+            panel2.Location = new Point(260, 76);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(280, 1);
+            panel2.TabIndex = 1;
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.BackColor = Color.FromArgb(15, 15, 15);
+            txtUsuario.BorderStyle = BorderStyle.None;
+            txtUsuario.Font = new Font("Times New Roman", 12F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            txtUsuario.ForeColor = Color.DimGray;
+            txtUsuario.Location = new Point(260, 57);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(280, 19);
+            txtUsuario.TabIndex = 0;
+            txtUsuario.TabStop = false;
+            txtUsuario.Text = "Usuario";
+            txtUsuario.Enter += txtUsuario_Enter;
+            txtUsuario.Leave += txtUsuario_Leave;
+            // 
+            // txtPassword
+            // 
+            txtPassword.BackColor = Color.FromArgb(15, 15, 15);
+            txtPassword.BorderStyle = BorderStyle.None;
+            txtPassword.Font = new Font("Times New Roman", 12F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            txtPassword.ForeColor = Color.DimGray;
+            txtPassword.Location = new Point(260, 124);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(280, 19);
+            txtPassword.TabIndex = 1;
+            txtPassword.TabStop = false;
+            txtPassword.Text = "Password";
+            txtPassword.Enter += txtPassword_Enter;
+            txtPassword.Leave += txtPassword_Leave;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.DimGray;
+            panel3.Enabled = false;
+            panel3.Location = new Point(260, 143);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(280, 1);
+            panel3.TabIndex = 3;
+            // 
+            // lblLogin
+            // 
+            lblLogin.AutoSize = true;
+            lblLogin.Font = new Font("Times New Roman", 20.25F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            lblLogin.ForeColor = Color.DimGray;
+            lblLogin.Location = new Point(358, 9);
+            lblLogin.Name = "lblLogin";
+            lblLogin.Size = new Size(80, 31);
+            lblLogin.TabIndex = 5;
+            lblLogin.Text = "Login";
+            // 
+            // btnAcceder
+            // 
+            btnAcceder.BackColor = Color.FromArgb(40, 40, 40);
+            btnAcceder.FlatAppearance.BorderSize = 0;
+            btnAcceder.FlatAppearance.MouseDownBackColor = Color.FromArgb(28, 28, 28);
+            btnAcceder.FlatAppearance.MouseOverBackColor = Color.FromArgb(64, 64, 64);
+            btnAcceder.FlatStyle = FlatStyle.Flat;
+            btnAcceder.Font = new Font("Times New Roman", 12F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            btnAcceder.ForeColor = Color.LightGray;
+            btnAcceder.Location = new Point(260, 178);
+            btnAcceder.Name = "btnAcceder";
+            btnAcceder.Size = new Size(280, 30);
+            btnAcceder.TabIndex = 2;
+            btnAcceder.TabStop = false;
+            btnAcceder.Text = "Acceder";
+            btnAcceder.UseVisualStyleBackColor = false;
+            // 
+            // btnCerrar
+            // 
+            btnCerrar.Image = Properties.Resources.delete;
+            btnCerrar.Location = new Point(598, 0);
+            btnCerrar.Name = "btnCerrar";
+            btnCerrar.Size = new Size(20, 20);
+            btnCerrar.SizeMode = PictureBoxSizeMode.Zoom;
+            btnCerrar.TabIndex = 7;
+            btnCerrar.TabStop = false;
+            btnCerrar.Click += btnCerrar_Click;
+            // 
+            // btnMinimizar
+            // 
+            btnMinimizar.Image = (Image)resources.GetObject("btnMinimizar.Image");
+            btnMinimizar.Location = new Point(578, 0);
+            btnMinimizar.Name = "btnMinimizar";
+            btnMinimizar.Size = new Size(18, 20);
+            btnMinimizar.SizeMode = PictureBoxSizeMode.Zoom;
+            btnMinimizar.TabIndex = 8;
+            btnMinimizar.TabStop = false;
+            btnMinimizar.Click += btnMinimizar_Click;
             // 
             // Login
             // 
             AutoScaleMode = AutoScaleMode.None;
-            ClientSize = new Size(530, 330);
+            BackColor = Color.FromArgb(15, 15, 15);
+            ClientSize = new Size(620, 280);
+            Controls.Add(btnMinimizar);
+            Controls.Add(btnCerrar);
+            Controls.Add(btnAcceder);
+            Controls.Add(lblLogin);
+            Controls.Add(txtPassword);
+            Controls.Add(panel3);
+            Controls.Add(txtUsuario);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "Login";
+            Opacity = 0.9D;
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Login";
+            MouseDown += Login_MouseDown;
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)btnCerrar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)btnMinimizar).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private TextBox txtUsuario;
+        private TextBox txtPassword;
+        private Panel panel3;
+        private Label lblLogin;
+        private Button btnAcceder;
+        private PictureBox pictureBox1;
+        private PictureBox btnCerrar;
+        private PictureBox btnMinimizar;
     }
 }
